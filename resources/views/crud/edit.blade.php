311342ce101840b42c4cc6c15012871256311566
mess-dev <stephane.maissin@gmail.com>
2022-05-22 12:25:54 +0100

    =Init

--- resources/views/crud/edit.blade.php
+++ resources/views/crud/edit.blade.php
@@ -10,7 +10,7 @@
 		<div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
 			<a href="{{ route($routePrefix . Str::plural(Str::slug($class)) . '.index', [$class => $entry]) }}"
 			   class="inline-flex items-center justify-center rounded-md border border-transparent bg-yellow-600 px-4 py-2 text-md font-medium text-white shadow-sm hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 sm:w-auto">
-				{{ __('List '. Str::replace('_', ' ', Str::title(Str::snake(Str::Studly(\Str::plural($class)))))) }}
+				{{ __('labels.List '. Str::replace('_', ' ', Str::title(Str::snake(Str::Studly(\Str::plural($class)))))) }}
 			</a>
 		</div>
 	</div>
